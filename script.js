d90d18fe631c00f9a8e2fe4ea7dd7f215b6744cc
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-04-16 00:10:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const text = document.createTextNode("DOM load success");
   document.body.appendChild(text);
 });
